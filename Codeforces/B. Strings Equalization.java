4d0a352c0bfe6088fabbd02f5cb9c284bc2728c9
ZH Rifat <45078260+zh-rifat@users.noreply.github.com>
2022-01-03 06:14:03 +0600

    Create B. Strings Equalization.java

---
+++ Codeforces/B. Strings Equalization.java
@@ -0,0 +1,27 @@
+/*********************************************/                                        
+/* Author   : ZH Rifat (zhrifat.00@gmail.com)*/                                                
+/*********************************************/
+
+import java.util.Scanner;
+ 
+public class CF_1241B {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int q=input.nextInt();
+        while(q-->0){
+            String s=input.next(),t=input.next();
+            char[] sc=s.toCharArray(),tc=t.toCharArray();
+            int eq=0;
+            for(int i=0;i<sc.length;i++){
+                for(int j=0;j<tc.length;j++){
+                    if(sc[i]==tc[j]){
+                        eq++;
+                        break;
+                    }
+                }
+            }
+            if(eq>0) System.out.println("YES");
+            else System.out.println("NO");
+        }
+    }
+}
