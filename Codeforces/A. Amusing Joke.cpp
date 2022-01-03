e06e5ad7562c1d7c9236121b934006b8b8f24504
ZH Rifat <45078260+zh-rifat@users.noreply.github.com>
2022-01-03 06:19:57 +0600

    Create A. Amusing Joke.cpp

---
+++ Codeforces/A. Amusing Joke.cpp
@@ -0,0 +1,27 @@
+/*********************************************/                                        
+/* Author   : ZH Rifat (zhrifat.00@gmail.com)*/                                                
+/*********************************************/
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string a,b,s;
+    cin>>a>>b>>s;
+    string z=a+b,f="";
+    if(z.length()==s.length())
+    for(int i=0;i<s.length();i++){
+        for(int j=0;j<z.length();j++){
+            if(s[i]==z[j]){
+                z[j]=0;
+                f+=s[i];
+                break;
+            }
+        }
+    }
+    if(s==f)
+        cout<<"YES";
+    else
+        cout<<"NO";
+ 
+    return 0;
+}
